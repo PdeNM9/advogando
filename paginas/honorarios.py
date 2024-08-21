d72443b88a12d12f925f671687ebf226f3b195f7
PdeNM9 <118948900+PdeNM9@users.noreply.github.com>
2024-08-21 01:01:41 +0000

    Alteração checkbox honorários e index

--- paginas/honorarios.py
+++ paginas/honorarios.py
@@ -21,7 +21,6 @@ def formatar_valor(valor):
     valor_formatado = f"{valor:,.2f}"
     return "R$ " + valor_formatado.replace(',', 'X').replace('.', ',').replace('X', '.')
 
-
 # Converte o valor para texto por extenso em português
 def valor_por_extenso(valor):
     return num2words(valor, lang='pt_BR', to='currency')
@@ -30,94 +29,79 @@ def valor_por_extenso(valor):
 processo = st.text_input('Insira o número do processo:', placeholder='Ex: 0123456-00.2099.8.05.0001')
 autor = st.text_input('Insira o nome do autor:', placeholder='Nome do autor.')
 valor_total_creditado = st.number_input('Insira o valor total creditado na conta do escritório (R$):', placeholder='R$ 0,00')
-porcentagem_sucumbencia = st.number_input('Insira a porcentagem dos honorários de sucumbência (%):', min_value=0)
 porcentagem_contratual = st.number_input('Insira a porcentagem dos honorários contratuais (%):', min_value=0)
-quantidade_advogados = st.number_input('Insira a quantidade de advogados', min_value=1)
+
+# Checkboxes para campos opcionais
+calcular_sucumbencia = st.checkbox('Incluir honorários de sucumbência?')
+
+if calcular_sucumbencia:
+    porcentagem_sucumbencia = st.number_input('Insira a porcentagem dos honorários de sucumbência (%):', min_value=0)
+else:
+    porcentagem_sucumbencia = 0  # Define como 0 se não for calcular
+
+mais_advogados = st.checkbox('Dividir honorários entre advogados?')
+
+if mais_advogados:
+    quantidade_advogados=st.number_input('Insira a quantidade de advogados',min_value=2)
+else:
+    quantidade_advogados = 1  # Define como 1 se não houver divisão
+
 
 # Botão de cálculo e lógica
 if st.button('Calcular'):
     try:
-      valor_formatado = formatar_valor(valor_total_creditado)
-      valor_extenso = valor_por_extenso(valor_total_creditado)
-      st.write(f"Valor creditado: {valor_formatado} ({valor_extenso})")
+        valor_formatado = formatar_valor(valor_total_creditado)
+        valor_extenso = valor_por_extenso(valor_total_creditado)
+        st.write(f"Valor creditado: {valor_formatado} ({valor_extenso})")
     except ValueError:
         st.error("Por favor, insira um número válido para o valor total creditado.")
 
     if validar_processo(processo):
-        # Restante do código de cálculo, assumindo que o valor total e o número do processo são válidos.
-        pass
+        # Cálculos principais
+        valor_condenacao = valor_total_creditado / (1 + porcentagem_sucumbencia / 100)
+        honorarios_sucumbencia = calcular_sucumbencia and calcular_honorario(valor_condenacao, porcentagem_sucumbencia) or 0
+        honorarios_contratuais = calcular_honorario(valor_condenacao, porcentagem_contratual)
+        valor_para_parte = valor_condenacao - honorarios_contratuais
+        valor_escritorio = honorarios_sucumbencia + honorarios_contratuais
+        divisao_honorarios = valor_escritorio / quantidade_advogados
+
+        # Criando tabela com os resultados
+        dados = {
+            'Descrição': [
+                'Valor depositado:',
+                f'Honorários de Sucumbência: ({porcentagem_sucumbencia}%.)',
+                'Valor da Condenação:',
+                f'Honorários Contratuais: ({porcentagem_contratual}% sobre a condenação.)',
+                'Valor a ser transferido para a parte:',
+                'Valor restante do escritório:',
+                f'Valor para cada advogado: ({quantidade_advogados})'
+            ],
+            'Valor': [
+                formatar_valor(valor_total_creditado),
+                formatar_valor(honorarios_sucumbencia),
+                formatar_valor(valor_condenacao),
+                formatar_valor(honorarios_contratuais),
+                formatar_valor(valor_para_parte),
+                formatar_valor(valor_escritorio),
+                formatar_valor(divisao_honorarios)
+            ]
+        }
+
+        st.divider()
+
+        st.write(f'Processo nº: {processo}')
+        st.write(f'Autor: **{autor}**')
+        tabela = pd.DataFrame(dados)
+        st.table(tabela)
+
+        # Criação de CSV e botão de download
+        header_string = f"Processo nº: {processo}.\nAutor: {autor}.\n"
+        csv_data = tabela.to_csv(index=False)
+        full_csv = header_string + csv_data
+        csv_bytes = ('\ufeff' + full_csv).encode('utf-8')
+        st.download_button(label='Baixar tabela', data=csv_bytes, file_name='tabela.csv', mime='text/csv')
+
+        st.divider()
     else:
         st.error("Formato inválido! Por favor, insira o número no formato 0123456-00.2099.8.05.0001.")
 
-
-    # Calculando a sucumbência sobre a condenação.
-    valor_condenacao = valor_total_creditado / (1 + porcentagem_sucumbencia / 100)
-
-    # Calculando o valor dos honorários de sucumbência
-    honorarios_sucumbencia = calcular_honorario(valor_condenacao,
-                                                porcentagem_sucumbencia)
-
-    # Calculando honorários contratuais sobre a condenação
-    honorarios_contratuais = calcular_honorario(valor_condenacao,
-                                                porcentagem_contratual)
-
-    # Calculando o valor a ser transferido para a parte
-    valor_para_parte = valor_condenacao - honorarios_contratuais
-
-    # Valor que restará ao escritório
-    valor_escritorio = honorarios_sucumbencia + honorarios_contratuais
-
-    # Dividindo o total de honorários pela quantidade de advogados
-    divisao_honorarios = valor_escritorio / quantidade_advogados
-
-    # Criando tabela com os resultados
-
-    dados = {
-        'Descrição': [
-            'Valor depositado:',
-            f'Honorários de Sucumbência: ({porcentagem_sucumbencia}%.)',
-            'Valor da Condenação:',
-            f'Honorários Contratuais: ({porcentagem_contratual}% sobre a condenação.)',
-            'Valor a ser transferido para a parte:',
-            'Valor restante do escritório:',
-            f'Valor para cada advogado: ({quantidade_advogados})'
-        ],
-        'Valor': [
-            formatar_valor(valor_total_creditado),
-            formatar_valor(honorarios_sucumbencia),
-            formatar_valor(valor_condenacao),
-            formatar_valor(honorarios_contratuais),
-            formatar_valor(valor_para_parte),
-            formatar_valor(valor_escritorio),
-            formatar_valor(divisao_honorarios)
-        ]
-    }
-
-    st.divider()
-
-    st.write(f'Processo nº: {processo}')
-    st.write(f'Autor: **{autor}**')
-    tabela = pd.DataFrame(dados)
-    st.table(tabela)
-
-    # Cria a string de cabeçalho personalizada com as variáveis globais     
-
-    header_string = f"Processo nº: {processo}.\nAutor: {autor}.\n"
-
-    # Converte DataFrame para CSV     
-
-    csv_data = tabela.to_csv(index=False)
-
-    # Concatena o cabeçalho personalizado com os dados do CSV     
-
-    full_csv = header_string + csv_data
-
-    # Converte para bytes incluindo o BOM     
-
-    csv_bytes = ('\ufeff' + full_csv).encode('utf-8')
-
-    # Botão para baixar a tabela como CSV     
-
-    st.download_button(label='Baixar tabela', data=csv_bytes, file_name='tabela.csv', mime='text/csv')
-
-    st.divider()
--- paginas/index.py
+++ paginas/index.py
@@ -4,19 +4,21 @@ st.write("# Bem-Vindo ao Advogando! 👋")
 
 st.markdown(
   """
-  Advogando é um an open-source app framework built specifically for
-  Machine Learning and Data Science projects.
-  **👈 Select a demo from the sidebar** to see some examples
-  of what Streamlit can do!
-  ### Want to learn more?
-  - Check out [streamlit.io](https://streamlit.io)
-  - Jump into our [documentation](https://docs.streamlit.io)
-  - Ask a question in our [community
-      forums](https://discuss.streamlit.io)
-  ### See more complex demos
-  - Use a neural net to [analyze the Udacity Self-driving Car Image
-      Dataset](https://github.com/streamlit/demo-self-driving)
-  - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-  """
+O **Advogando** é a sua plataforma online de ferramentas jurídicas, projetada especialmente para facilitar o dia a dia dos advogados. Aqui, você encontra soluções práticas e eficientes para as suas necessidades, incluindo uma **Calculadora de Honorários Advocatícios** e uma **Calculadora de Prazos Processuais**. 
+  
+### **Calculadora de Honorários Advocatícios!** 
+A nossa **Calculadora de Honorários** foi desenvolvida para ajudar você a calcular com precisão os valores dos honorários contratuais e de sucumbência. Com uma interface intuitiva e funcionalidade flexível, você pode ajustar facilmente os parâmetros de acordo com cada caso específico. Além disso, a calculadora permite a personalização dos valores para múltiplos advogados, garantindo uma divisão justa e transparente dos honorários. 
+
+### **Calculadora de Prazos Processuais!** 
+Não perca prazos importantes! Com a **Calculadora de Prazos Processuais**, você pode inserir a data de publicação e o número de dias, e o sistema calculará automaticamente o prazo final, considerando suspensões e feriados. Esta ferramenta é essencial para garantir a gestão eficaz dos seus processos e evitar complicações devido a prazos não cumpridos.
+
+### **Por que usar o Advogando?** 
+* **Precisão:** Ferramentas desenvolvidas para fornecer cálculos detalhados e corretos. 
+* **Eficiência:** Economize tempo com uma plataforma que automatiza cálculos complexos. 
+* **Acessibilidade:** Acesse suas ferramentas jurídicas em qualquer lugar, a qualquer momento. 
+* **Facilidade de uso:** Interface amigável e intuitiva, sem necessidade de habilidades técnicas avançadas. 
+
+Explore nossas ferramentas e descubra como o **Advogando** pode simplificar sua rotina jurídica. 
+Estamos aqui para ajudar você a focar no que realmente importa: o sucesso de seus clientes."""
   )
 
